8ea1a6030fdc0c2d11df615e33e96ecdb9f96da8
thorondir <yingstokes@gmail.com>
2019-02-14 09:21:03 +1030

    basic skeletons of functions

--- networking/networking.py
+++ networking/networking.py
@@ -1,12 +1,22 @@
 import socket, struct
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+IP = ""
+PORT = 4203
 
-
-def join():
-
+def join(iptojoin, name):
+    global IP
+    IP = iptojoin
+    socket.sendto( , (IP, PORT)) # send the server a message telling it to create a new player with name
 def leave():
-
-def send():
-
-def receive():
+    socket.sendto( , (IP, PORT)) # message telling server to delete this player.
+    #INPUTS:  bool{KEY1, KEY2, KEY3, KEY4, UP, DOWN, LEFT, RIGHT}
+def send_input(inputs):
+    msg = '' 
+    for bit in inputs:
+        if bit: msg += '1'
+        else: msg += '0'
+    msg = int(msg,2)
+    sock.sendto(bytes([msg]), (IP, PORT))
+            
+def receive_state():
